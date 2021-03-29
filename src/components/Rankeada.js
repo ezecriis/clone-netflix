7cb22b1cfbc2a709b8f3664db98ca0aefb5d5f32
Cristian Ledesma <cr.ledesma@hotmail.com>
2021-03-29 18:01:56 -0300

    Updated changes

---
+++ src/components/Rankeada.js
@@ -0,0 +1,83 @@
+import React, { Component } from "react"
+import Loader from "./Loader"
+import Icon from "./Icon"
+
+const API_URL = 'https://api.themoviedb.org/3/movie/now_playing?api_key=';
+const KEY = '6f26fd536dd6192ec8a57e94141f8b20';
+
+class Rankeada extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+      isLiked: false
+    }
+  }
+
+  componentDidMount() {
+    fetch(API_URL + KEY)
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          items: json,
+        })
+      })
+  }
+
+  render() {
+    var { isLoaded, items } = this.state
+
+    if (!isLoaded) {
+      return (
+        <div className="movie-wrapper" style={{ height: 100 }}>
+          <Loader />
+        </div>
+      )
+    } else {
+      let base_url = "https://image.tmdb.org/t/p/"
+      let item = items.results[this.props.index]
+      let title = item.original_title
+      let coverImg = base_url + "/w780/" + item.poster_path
+      let isAdult = item.adult
+
+      return (
+        <div className="movie-wrapper">
+          <img
+            className="movie-cover"
+            src={coverImg}
+            index={this.props.index}
+            alt={title + "cover"}
+          />
+          <div className="on-hover">
+            <button className="wrap-circle" aria-label="Reproducir">
+              <Icon icon="play"></Icon>
+              <div className="circle"></div>
+            </button>
+
+            <div className="middle-wrapper">
+              <div className="details">
+                <h3 className="movie-title"> {title} </h3>
+                <p className="movie-details">
+                  98% Coincidencia
+                  <span className="age-rating"> {isAdult ? "+16" : "ATP"} </span>
+                  1 h 30 min <br /> Suspenso
+                  </p>
+              </div>
+              <button className="wrap-circle" aria-label="Me gusta">
+                <Icon icon="like"></Icon>
+                <div className="circle"></div>
+              </button>
+            </div>
+            <button className="big-arrow">
+              <Icon icon="arrow" />
+            </button>
+          </div>
+        </div>
+      )
+    }
+  }
+}
+
+export default Rankeada
